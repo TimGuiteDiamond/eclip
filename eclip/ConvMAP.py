12cd5840ff41da3ad9bfa77253401b927ed2e794
JennaElli <jennamelliott@gmail.com>
2018-07-11 11:14:05 +0100

    comments for log file

--- eclip/ConvMAP.py
+++ eclip/ConvMAP.py
@@ -4,80 +4,125 @@ import os
 from PIL import Image as pimg
 import shutil # dont know what this does
 import sys #dont know what this does
-
+import scipy.ndimage
 import numpy as np
 #mrcfile is a CCP-EM library for i/o to MRC format map files
 import mrcfile 
 
 ###################################################################
-#we need a set of conditions
-#axis to section along
-#axes_list = ['X','Y','Z']
-#linear dimension of slice
-#window_size=20
-#offset from one slice to the next within a section
-#offset = 10 
-#offset from one section to the next 
-#section_skip =2
-#normalise values in a slice to be between map_min and map_max
-#normalise = True
-#map_min =np.amin(mrc.data)
-#map_max=np.amax(mrc.data)
-#map_min = -2.14
-#map_max = 4.19
-#threshold for counting ref map density
-#ref_thresh = 0.1
-#if a fraction of grip points in reference map have significant density, then
-#label as good
-#good_fraction = 0.05
+'''arguments for image_slicing:
+input_map_directory: this is a string indicating where to find the .map files
+output_directory: this is a string indicating where to save the image files
+
+axes_list: axis to section along (default: axes_list = ['X','Y','Z'])
+
+window_size: the linear dimension of the scanning window for each slice
+(default:  window_size=20)
+
+offset: the offset distance between each window position. (default: offset = 10
+)
+
+section_skip: the separation between each section slice. (default: section_skip
+=2)
+
+normalise: boolean, decide whether to normalise values in a slice to be between
+map_min and map_max (default: normalise = True)
+
+map_min: minimum value for the map (default: map_min = -2.14)
+map_max: maximum vlaue for the map (default: map_max =  4.19)
+blur: boolean, decide whether to blur the data via a gaussian filter. (default:
+blur = True)
+sigma: if blur is True, this is the standard deviation for the gaussian.
+(default: sigma = 2.5)
+'''
 ########################################################################
-#as a function?
-
-
 def image_slicing(input_map_directory, output_directory, axes_list =
-['X','Y','Z'],window_size=20,offset =
-10,section_skip=2,normalise = True ,map_min=-2.14,map_max=4.19):
+['X','Y','Z'],window_size=10,offset =
+10,section_skip=2,normalise = True
+,map_min=-2.14,map_max=4.19,blur=True,sigma=2.5):
+
+  #checking output_directory exists
+  if not os.path.isdir(output_directory):
+    print("creating output directory "+output_directory)
+    os.mkdir(output_directory)
+
+  #creating logfile  
+  text = open(os.path.join(output_directory,'logfile_image_slicing.txt'),'a')
+  text.write('this is a log file for the progress of image_slicing(). \n')
 
   #opening file
-  print("opening file..")
   for file in os.listdir(input_map_directory):
     f=str(file)
     mrc = mrcfile.open(os.path.join(input_map_directory,f))
-    mrc.print_header()
+    #mrc.print_header()
+    print('Opening file')
     file_name = os.path.splitext(f)[0]
-  
-  #ensuring the output directory exists
-    if not os.path.isdir(output_directory):
-      print("creating output directory...")
-      os.mkdir(output_directory)
+    print(file_name)
+    text.write('for file'+file_name+'.\n\n Opening file...\n')
+
+    if file_name.endswith('_i'):
+      protein=file_name[:-2]
+    else:
+      protein=file_name
+    print(protein)
+
+    output_dir1 = os.path.join(output_directory,protein)
+    #ensuring the output directory exists
+    if not os.path.isdir(output_dir1):
+      print("creating output directory "+output_dir1)
+      text.write('creating output directory '+output_dir1+'\n')
+      os.mkdir(output_dir1)
+
+    output_dir2 = os.path.join(output_dir1,file_name)
+    if not os.path.isdir(output_dir2):
+      print("creating output directory "+ output_dir2)
+      text.write('creating output directory '+output_dir2+'\n')
+      os.mkdir(output_dir2)
     
  #   '''the value of axis_list was specified above, we need to loop over the axes
  #   specified in this list and transpose the data columns accordingly- we need the
  #   sectioning axis to be in mrc.data[2]'''
     
     for axis in axes_list:
-      if axis in ['X']: 
+      if axis in ['X']:
+        text.write('\nFor the '+axis+' axis. \n\n')
         print("transposing")
-        loc_mrcdata=np.transpose(mrc.data,(1,2,0))
+        text.write('transposing...\n')
+        loc_mrcdata_a=np.transpose(mrc.data,(1,2,0))
       elif axis in ['Y']:
-        loc_mrcdata=np.transpose(mrc.data,(2,0,1))
+        loc_mrcdata_a=np.transpose(mrc.data,(2,0,1))
       else:
-        loc_mrcdata=mrc.data
-    
-      assert window_size < loc_mrcdata.shape[1]
-      assert window_size < loc_mrcdata.shape[0]
-  
+        loc_mrcdata_a=mrc.data
+      
+      output_dir3 = os.path.join(output_dir2,axis)
+      if not os.path.isdir(output_dir3):
+        print("creating output directory "+output_dir3)
+        text.write('creating output directory '+output_dir3+'\n')
+        os.mkdir(output_dir3)
+
 #'''a variable to log image file names in order- completely optional, but can
 #be helpful.'''
+       #bluring the data using a gaussian filter
+      if blur:
+        loc_mrcdata=scipy.ndimage.filters.gaussian_filter(loc_mrcdata_a,(sigma,sigma,sigma))
+        print('bluring map')
+        text.write('bluring map\n')
+      else:
+        loc_mrcdata=loc_mrcdata_a
+     
       num=0
-    
       for section in range(0,loc_mrcdata.shape[2], section_skip):
         print("processing section",section)
+        text.write('\nprocessing section '+str(section)+'\n')
         num +=1
         myim = loc_mrcdata[:,:,section].copy(order='C')
-         
+        
+        #to normalise the contrast of the image
         if normalise:
+         print('normalising')
+         text.write('normalising\n')
          myim = 255.0*np.clip((myim-map_min)/(map_max-map_min),0,1)
+
         #this is to same the files in order and otherwise unimportant
         if num < 10:
          imgout_filename = file_name +'_00'+str(num)+'_'+str(section)+axis+'.png'
@@ -85,19 +130,28 @@ def image_slicing(input_map_directory, output_directory, axes_list =
          imgout_filename = file_name +'_0'+str(num)+'_'+str(section)+axis+'.png'
         else:
           imgout_filename =file_name+'_'+str(num)+'_'+str(section)+axis+'.png'
+        #saving file
         img_out = pimg.fromarray(myim)
         img_new =  img_out.convert('RGB')
-        img_new.save(output_directory+'/'+imgout_filename)
+        img_new.save(output_dir3+'/'+imgout_filename)
+        print('saved image: '+imgout_filename+' in directory '+output_dir3+'\n')
+        text.write('saved image: '+imgout_filename+' in directory '+output_dir3+'\n')
        
     
-    print("Finished. The images are saved in: ",output_directory )
-
+    print("Finished. The images are saved in ",output_dir1 )
+    text.write('Finished. The images are saved in ",output_dir1')
+    text.close()
 ##########################################################################################
 #input and output directories
-input_map_directory1 = '/dls/science/users/ycc62267/mapfdr/tests'
-output_directory1 = '/dls/science/users/ycc62267/imgfdr/overall/noblur'
-
-image_slicing(input_map_directory1,output_directory1)
+#input_map_directory1 = '/dls/science/users/ycc62267/mapfdr/gauss'
+#output_directory1 = '/dls/science/users/ycc62267/imgfdr/gauss/blur25'
+#output_directory3 = '/dls/science/users/ycc62267/imgfdr/gauss/blur10'
+#output_directory2 = '/dls/science/users/ycc62267/imgfdr/gauss/noblur'
+#
+#
+#image_slicing(input_map_directory1,output_directory1)
+#image_slicing(input_map_directory1,output_directory2,blur=False)
+#image_slicing(input_map_directory1, output_directory3,sigma=10)
 
   
 
@@ -110,6 +164,11 @@ image_slicing(input_map_directory1,output_directory1)
 
 '''this section makes a scanning window of images, but is not necessary to run the
       code at the moment.'''
+
+        #assert window_size < loc_mrcdata.shape[1]
+        #assert window_size < loc_mrcdata.shape[0]
+
+
        # for col in range(0,loc_mrcdata.shape[1]-window_size,offset):
        #   for row in range(0,loc_mrcdata.shape[0]-window_size,offset):
     
