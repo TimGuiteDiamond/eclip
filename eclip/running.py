0142c3a4dc8684e0ab51d075d69eac15b1fc3eb5
JennaElli <jennamelliott@gmail.com>
2018-07-11 11:12:58 +0100

    creates a logfile of progress

---
+++ eclip/running.py
@@ -0,0 +1,12 @@
+#this is an attempt to call a function from another folder
+
+from ConvMAP import image_slicing
+
+input_map_directory1='/dls/science/users/ycc62267/mapfdr/gauss'
+
+output_directory1='/dls/science/users/ycc62267/imgfdr/gauss/blurdir'
+
+image_slicing(input_map_directory1,output_directory1)
+
+#sorting output directories
+
