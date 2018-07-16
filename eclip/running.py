1f4749cbd6fd52e98b51d83430e136fab206c6dc
JennaElli <jennamelliott@gmail.com>
2018-07-16 17:07:35 +0100

    runs with identially sized images

--- eclip/running.py
+++ eclip/running.py
@@ -2,9 +2,9 @@
 
 from ConvMAP import image_slicing
 
-input_map_directory1='/dls/mx-scratch/ycc62267/mapfdr'
+input_map_directory1='/dls/mx-scratch/ycc62267/mapfdrbox'
 
-output_directory1='/dls/mx-scratch/ycc62267/imgfdr/blur2_5'
+output_directory1='/dls/mx-scratch/ycc62267/imgfdr/blur2_5_maxminbox'
 
 image_slicing(input_map_directory1,output_directory1)
 
