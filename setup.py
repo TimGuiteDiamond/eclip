f1342a06c834cd5bce14957b99c1eb08581dbcc6
JennaElli <jennamelliott@gmail.com>
2018-08-24 15:00:52 +0100

    setup stuff

--- setup.py
+++ setup.py
@@ -5,17 +5,34 @@ setup(
     version = "0.0.1",
     author = "Jenna Elliott",
     author_email = "jenna.elliott@diamond.ac.uk",
-    description = "A package that classifies maps",
+    description = "A proof of principle package that applies machine learning to classify electron density maps",
     license = "BSD",
-    keywords = "awesome python package",
+    keywords = "electron density classification",
     packages=[
       'eclip', 
       'tests'
     ],
     scripts=[
+      'bin/RunTrain',
+      'bin/RunPred',
+      'bin/ConvMAP',
+      'bin/learn',
+      'bin/EP_success',
+      'bin/predic',
+      'bin/predictest'
     ],
     install_requires=[
       'pytest',
+      'keras',
+      'tensorflow',
+      'matplotlib',
+      'scikit-learn',
+      'Pillow',
+      'numpy',
+      'scipy',
+      'mrcfile',
+      'logging',
+      'argparse'
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
